5ced786147828fca3f49424cc6a32fd9626ce7e2
miz <miz1955a6@gmail.com>
2025-12-08 08:31:55 +0900

    formでブラウザにより正常動作しない問題に対応

--- app/_components/ContactGoogleForm/index.tsx
+++ app/_components/ContactGoogleForm/index.tsx
@@ -6,16 +6,21 @@ import { useState, useRef } from "react";
 export default function ContactGoogleForm() {
     const [flag, setFlag] = useState<boolean>(false);
     const iframeRef = useRef<HTMLIFrameElement | null>(null);
+    const isSubmitRef = useRef(false);
+    const handleSubmit = () => {
+        isSubmitRef.current = true;
+    };
     const handleIframeLoad = () => {
         const iframe = iframeRef.current;
-        if (!iframe || iframe.src === "about:blank") return;
+        if (!iframe || !isSubmitRef.current) return;
+        isSubmitRef.current = false;
         setFlag(true);
     };
     return (
         <>
         {!flag && (
             <div className={styles.contactForm}>
-                <form action="https://docs.google.com/forms/u/0/d/e/1FAIpQLScOzFDLBpMjpmbGyppixJhIo3sx1lvOAeq82gr_4YNi0ndHcw/formResponse" method="POST" id="mG61Hd" target="hiddenIframe">
+                <form action="https://docs.google.com/forms/u/0/d/e/1FAIpQLScOzFDLBpMjpmbGyppixJhIo3sx1lvOAeq82gr_4YNi0ndHcw/formResponse" method="POST" id="mG61Hd" target="hiddenIframe" onSubmit={handleSubmit}>
                     <div className={styles.contactFormInput}>
                         <label htmlFor="contactFormName"><span className={styles.required}>必須</span>お名前</label>
                         <br />
